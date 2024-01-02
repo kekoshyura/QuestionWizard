8e05f4a109433f837719956e7ef48ece640089b0
dimaxalva <96759517+dimaxalva@users.noreply.github.com>
2024-01-02 20:13:08 +0200

    seeding identity data

---
+++ Server/Data/SeedAdministratorRoleAndUser.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Server.Data;
+public static class SeedAdministratorRoleAndUser {
+    internal const string AdministratorRoleName = "Administrator";
+    internal const string AdministratorUserName = "admin@questionWizzard.com";
+
+    internal async static Task Seed(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager) {
+        await SeedAdministratorRole(roleManager);
+        await SeedAdministratorUser(userManager);
+    }
+
+    private async static Task SeedAdministratorRole(RoleManager<IdentityRole> roleManager) {
+
+        bool administratorRoleExist = await roleManager.RoleExistsAsync(AdministratorRoleName);
+        if (administratorRoleExist == false) {
+            var role = new IdentityRole {
+                Name = AdministratorRoleName
+            };
+
+            await roleManager.CreateAsync(role);
+        }
+    }
+
+    private async static Task SeedAdministratorUser(UserManager<IdentityUser> userManager) {
+
+        bool administratorUserExist = await userManager.FindByEmailAsync(AdministratorUserName) != null;
+        if (administratorUserExist == false) {
+            var user = new IdentityUser {
+                UserName = AdministratorUserName,
+                Email = AdministratorUserName,
+            };
+
+            IdentityResult identityResult = await userManager.CreateAsync(user, "Password1!");
+            if (identityResult.Succeeded) {
+                await userManager.AddToRoleAsync(user, AdministratorRoleName);
+            }
+        }
+    }
+}
+
--- Server/Program.cs
+++ Server/Program.cs
@@ -46,15 +46,29 @@ builder.Services.AddCors(options => {
         .AllowAnyHeader());
 });
 
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    try {
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+
+        SeedAdministratorRoleAndUser.Seed(roleManager, userManager).Wait();
+    }
+    catch (Exception ex) {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while seeding the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseRouting();
